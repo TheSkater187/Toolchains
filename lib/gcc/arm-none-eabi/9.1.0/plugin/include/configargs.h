74e4b7300774b3dff3333284e14933f53a3e0efc
The~Skater~187 <Theskaterdad187@gmail.com>
2019-05-27 12:23:43 -0400

    Self-Compiled Bleeding-Edge Toolchain_9.1

---
+++ lib/gcc/arm-none-eabi/9.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/skater187/toolchains/b.e./sources/gcc-9.1.0/configure --target=arm-none-eabi --prefix=/home/skater187/toolchains/b.e./installNative --docdir=/home/skater187/toolchains/b.e./installNative/share/doc --libexecdir=/home/skater187/toolchains/b.e./installNative/lib --enable-languages=c,c++ --disable-libstdcxx-verbose --enable-plugins --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libssp --disable-libstdcxx-pch --disable-nls --disable-shared --disable-threads --disable-tls --with-gnu-as --with-gnu-ld --with-newlib --with-headers=yes --with-sysroot=/home/skater187/toolchains/b.e./installNative/arm-none-eabi --with-system-zlib --with-gmp=/home/skater187/toolchains/b.e./buildNative/prerequisites/gmp-6.1.2 --with-mpfr=/home/skater187/toolchains/b.e./buildNative/prerequisites/mpfr-4.0.2 --with-mpc=/home/skater187/toolchains/b.e./buildNative/prerequisites/mpc-1.1.0 --with-isl=/home/skater187/toolchains/b.e./buildNative/prerequisites/isl-0.21 --with-pkgversion=bleeding-edge-toolchain --with-multilib-list=rmprofile";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm7tdmi" }, { "float", "soft" } };
